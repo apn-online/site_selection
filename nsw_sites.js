ae76c92bbfebfd01dcfea4cca7f84562b6e9e5d6
Mark Gandolfo <mark.gandolfo@apn.com.au>
2015-03-10 14:26:38 +1000

    updated qld sites & nsw sites
    mend

--- nsw_sites.js
+++ nsw_sites.js
@@ -1,4 +1,4 @@
-ijavascript: (function() {
+javascript: (function() {
   var w = ['coffscoastadvocate.com.au', 'dailyexaminer.com.au', 'tweeddailynews.com.au', 'northernstar.com.au', 'ballinaadvocate.com.au', 'byronnews.com.au', 'echonews.com.au'];
   django.jQuery('select#id_sites > option,select#id_sites_from > option').each(function() {
     if (django.jQuery.inArray(django.jQuery(this).text().replace('www.', ''), w) >= 0) {
@@ -6,4 +6,3 @@ ijavascript: (function() {
     }
   });
 })();
-
